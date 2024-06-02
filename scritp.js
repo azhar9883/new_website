6e02721dc8aff12a975be904fae707543a774d43
Azhar9883 <ayankhan59470@gmail.com>
2024-06-02 12:27:33 +0530

    first commit

---
+++ scritp.js
@@ -0,0 +1,15 @@
+
+function show() {
+    console.log("hello")
+    var tog = document.getElementById('head_content');
+    if (tog.style.top === "-250px") {
+        tog.style.top = "89px";
+    } else {
+        tog.style.top = "-250px";
+    }
+}
+
+
+
+
+            
